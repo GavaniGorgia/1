0d67395d858c6ed5bf17ccf40af0a92c0085b13a
GavaniGorgia <lisovskiil@mail.ru>
2021-11-02 11:23:49 +0300

    first commit

--- app.py
+++ app.py
@@ -1,69 +1,69 @@
-from flask import Flask, render_template, request
-
-import requests
-import os
-import uuid
-import json
-from dotenv import load_dotenv
-load_dotenv()
+from flask import Flask, render_template, redirect,request
+import psycopg2
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+
+@app.route('/',methods=['GET'])
+def ref():
+    return redirect("/login/")
+
+
+@app.route('/login/', methods=['GET','POST'])
 def index():
-    return render_template('index.html')
-
-# code
-@app.route('/', methods=['POST'])
-def index_post():
- 
-    # Read the values from the form
-    original_text = request.form['text']
-    target_language = request.form['language']
-
-    # Load the values from .env
-    key = os.environ['KEY']
-    endpoint = os.environ['ENDPOINT']
-    location = os.environ['LOCATION']
-
-    # Indicate that we want to translate and the API
-    # version (3.0) and the target language
-    path = '/translate?api-version=3.0'
+    if request.method == 'POST':
+        if request.form.get('login'):
+            errors = ""
+            login = request.form.get('username').strip()
+            if login == "":
+                errors += "введите логин,вместо пустой строки <br>"
+            password = request.form.get('password').strip()
+            if password == "":
+                errors += "введите пароль,вместо пустой строки <br>" 
+            if errors != "" :
+                return errors 
+            conn = psycopg2.connect(database = "service",
+                                user="postgres",
+                                password="lolo2002",
+                                host="localhost",
+                                port="5432")
+            cursor = conn.cursor()
+            cursor.execute("SELECT name FROM users.user WHERE login=%s and password=%s",
+            (str(login), str(password)))
+            records = cursor.fetchall()
+            if records !="":
+                return render_template('account.html', full_name = records[0][0])
+            else:
+                return "Введите пароль или логин заново"
+        if request.form.get('registration'):
+            return redirect('/registration/')
     
-    # Add the target language parameter
-    target_language_parameter = '&to=' + target_language
-    
-    # Create the full URL
-    constructed_url = endpoint + path + target_language_parameter
+    return render_template('login.html')
 
-    # Set up the header information, which includes our
-    # subscription key
-    headers = {
-        'Ocp-Apim-Subscription-Key': key,
-        'Ocp-Apim-Subscription-Region': location,
-        'Content-type': 'application/json',
-        'X-ClientTraceId': str(uuid.uuid4())
-    }
+@app.route('/registration/', methods=['GET','POST'])
+def index2():
+    if request.method == 'POST':
+        errors = ""
+        name = request.form.get('name').strip()
+        if name == "":
+            errors+="введите имя,вместо пустой строки <br>"
+        login = request.form.get('login').strip()
+        if login == "":
+            errors+="введите логин,вместо пустой строки <br>"       
+        password = request.form.get('password').strip()
+        if password == "":
+            errors+= "введите пароль,вместо пустой строки <br>"
+        if errors != "" :
+            return errors            
+        conn = psycopg2.connect(database = "service",
+                                user="postgres",
+                                password="lolo2002",
+                                host="localhost",
+                                port="5432")
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO users.user (name,login,password) VALUES (%s,%s,%s );",
+        (str(name),str(login), str(password)))
+        conn.commit()
+        return redirect('/login/')
 
-    # Create the body of the request with the text to be
-    # translated
-    body = [{'text': original_text}]
-
-    # Make the call using post
-    translator_request = requests.post(
-        constructed_url, headers=headers, json=body)
-    
-    # Retrieve the JSON response
-    translator_response = translator_request.json()
-    
-    # Retrieve the translation
-    translated_text = translator_response[0]['translations'][0]['text']
-
-    # Call render template, passing the translated text,
-    # original text, and target language to the template
-    return render_template(
-        'results.html',
-        translated_text=translated_text,
-        original_text=original_text,
-        target_language=target_language
-    )
+    return render_template('registration.html')
